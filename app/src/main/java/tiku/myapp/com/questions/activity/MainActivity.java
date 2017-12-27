25b5e805b0f83ffea1f1076406fd796c038c339f
swh123118 <songwenhui@yunshuxie.com>
2017-12-27 16:40:50 +0800

    添加WheelView

--- app/src/main/java/tiku/myapp/com/questions/activity/MainActivity.java
+++ app/src/main/java/tiku/myapp/com/questions/activity/MainActivity.java
@@ -1,19 +1,31 @@
 package tiku.myapp.com.questions.activity;
 
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import tiku.myapp.com.questions.R;
+import tiku.myapp.com.questions.view.WheelView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private List<String> lists = new ArrayList<>();
+    private static final String[] PLANETS = new String[]{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Pluto"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +53,45 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,QuestionActivity.class));
+            }
+        });
+
+
+
+        findViewById(R.id.tv_chick_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chieckPop(lists);
+            }
+        });
+    }
+
+    private void chieckPop(List<String> lists) {
+        View contentView = LayoutInflater.from(MainActivity.this).inflate(
+                R.layout.pop_layout, null);
+        PopupWindow popupWindow = new PopupWindow(contentView,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupWindow.setFocusable(true);// 取得焦点
+        //注意  要是点击外部空白处弹框消息  那么必须给弹框设置一个背景色  不然是不起作用的
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+        //点击外部消失
+        popupWindow.setOutsideTouchable(true);
+        //设置可以点击
+        popupWindow.setTouchable(true);
+        //进入退出的动画
+        popupWindow.setAnimationStyle(R.style.mypopwindow_anim_style);
+
+        popupWindow.showAtLocation(contentView, Gravity.BOTTOM,0,0);
+
+        WheelView wva = (WheelView) contentView.findViewById(R.id.wheelview);
+
+        wva.setOffset(2);
+        wva.setItems(Arrays.asList(PLANETS));
+        wva.setOnWheelViewListener(new WheelView.OnWheelViewListener() {
+            @Override
+            public void onSelected(int selectedIndex, String item) {
+                Log.e("ddddddd", "selectedIndex: " + selectedIndex + ", item: " + item);
             }
         });
     }
